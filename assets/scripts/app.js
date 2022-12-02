0b01b52ed9e1fa3f4b9e5cda2d0d95c4cb3b7acf
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-12-02 15:01:40 -0800

    standard attack working.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,24 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK = 15;
 
 let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth = currentMonsterHealth - damage;
+  const playerDamange = dealPlayerDamage(MONSTER_ATTACK);
+  currentPlayerHealth = currentPlayerHealth - playerDamange;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('Player wins!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('Player lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('This is a draw!');
+  }
+}
+
+attackBtn.addEventListener('click', attackHandler);

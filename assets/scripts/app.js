fb3c49197938fa10c739892ab93c53248d9968bb
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-12-02 15:50:24 -0800

    strong attack working. refactored formulas.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10;
-const MONSTER_ATTACK = 15;
+const MONSTER_ATTACK_VALUE = 15;
+const STRONG_ATTACK_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -7,10 +8,16 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(attackMode) {
+  let maxDamage;
+  if (attackMode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (attackMode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth = currentMonsterHealth - damage;
-  const playerDamange = dealPlayerDamage(MONSTER_ATTACK);
+  const playerDamange = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth = currentPlayerHealth - playerDamange;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('Player wins!');
@@ -21,4 +28,13 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
